c72af1f984713f059d900aab8c3d58ac75cc8c03
Koding <karich@yahoo.com>
2015-06-06 15:46:29 +0000

    updated route to allow url to passed as parameter

--- app/controllers/browse_controller.rb
+++ app/controllers/browse_controller.rb
@@ -1,4 +1,7 @@
+require 'uri'
+
 class BrowseController < ApplicationController
   def index
+      @url = URI.decode(params[:url])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get '/:url' => 'browse#index'
+  get '/:url' => 'browse#index' , :constraints => { :url => /[^\/]+/ }
 
   get 'home/index'
 
